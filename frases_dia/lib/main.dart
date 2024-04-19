2b9483597eac11e37754cb3684e7cd588e9ba178
Sidh-1998 <sidiomarjgtba@gmail.com>
2024-04-19 13:40:05 -0300

    Estrutura inicial do app em flutter Formatação de Textos. aula 58

--- frases_dia/lib/main.dart
+++ frases_dia/lib/main.dart
@@ -3,28 +3,19 @@ import 'package:flutter/material.dart';
 void main(){
 
   runApp(
+
     MaterialApp(
-      title: "Frases do Dia",
-      home: Container(
-        color: Colors.greenAccent,
-        child: Column(
-          children: [
-            Text(
-                "Olá, Bebê.",
-              style: TextStyle(
-              fontSize: 40,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0,
-                wordSpacing: 0,
-                decoration: TextDecoration.underline,
-                decorationColor: Colors.cyan,
-                decorationStyle: TextDecorationStyle.solid,
-                color:  Colors.black
-            ),
-            )
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lista"),
+          backgroundColor: Colors.greenAccent,
         ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+               child: Text("Conteúdo Principal"),
+        ),
+        bottomNavigationBar: BottomAppBar(),
       ),
     ));
 }
