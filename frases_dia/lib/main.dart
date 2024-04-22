26070dc9227dfe259cd406de6c801fb46e470d80
Sidh-1998 <sidiomarjgtba@gmail.com>
2024-04-22 15:15:32 -0300

    Estrutura inicial do app em flutter Formatação de Textos. aula 64

--- frases_dia/lib/main.dart
+++ frases_dia/lib/main.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
+/*
+* Stateless -> Widgets que não podem ser Alterados (constantes)
+* Stateful -> Widgets que podem ser Alterados (variaveis)
+*/
+
 void main(){
 
   runApp(
 
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Lista"),
-          backgroundColor: Colors.greenAccent,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-               child: Text("Conteúdo Principal"),
-        ),
-        bottomNavigationBar: BottomAppBar(),
-      ),
+      home: HomeStateful(),
     ));
 }
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({super.key});
+
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  @override
+  Widget build(BuildContext context) {
+
+    var _texto = "Sidiomar";
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text ("titulo"),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: (){},
+              child: Text("Clique Aqui!"),
+            ),
+            Text("Nome: $_texto"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
